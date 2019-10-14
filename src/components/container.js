d0a53ccc4b6bfcd2347531ace79a41866517c63e
John Peterson <thejohnpg@gmail.com>
2019-10-14 17:00:14 -0300

    Fix: Container.js

--- src/components/container.js
+++ src/components/container.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/Textfield';
 import Button from '@material-ui/core/Button';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
 
 import api from './../api'
 import User from './User'
